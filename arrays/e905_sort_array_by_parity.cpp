08eea3953720a1694f919ef0528c5e0a949eb247
Liubov Sereda <lbvsereda@gmail.com>
2019-09-09 11:01:52 +0300

    added solution for sort_array_by_parity.cpp

---
+++ arrays/e905_sort_array_by_parity.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+#include "test_runner.h"
+
+static const auto io_sync_off = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return nullptr;
+}();
+
+class Solution {
+public:
+  vector<int> sortArrayByParity(vector<int>& A) {
+    vector<int> first;
+    vector<int> last;
+
+    for (int i : A) {
+      i % 2 == 0 ? first.push_back(i) : last.push_back(i);
+    }
+
+    for (int i : last) {
+      first.push_back(i);
+    }
+
+    return first;
+  }
+};
+
+void testSolution() {
+  Solution s;
+  {
+    vector<int> v = {3, 1, 2, 4};
+    vector<int> expected = {2, 4, 3, 1};
+    ASSERT_EQUAL(s.sortArrayByParity(v), expected);
+  }
+  {
+    vector<int> v = {3};
+    vector<int> expected = {3};
+    ASSERT_EQUAL(s.sortArrayByParity(v), expected);
+  }
+  {
+    vector<int> v = {};
+    vector<int> expected = {};
+    ASSERT_EQUAL(s.sortArrayByParity(v), expected);
+  }
+  {
+    vector<int> v = {3, 1, 7};
+    vector<int> expected = {3, 1, 7};
+    ASSERT_EQUAL(s.sortArrayByParity(v), expected);
+  }
+  {
+    vector<int> v = {2, 8, 10};
+    vector<int> expected = {2, 8, 10};
+    ASSERT_EQUAL(s.sortArrayByParity(v), expected);
+  }
+  {
+    vector<int> v = {3, 2, 4};
+    vector<int> expected = {2, 4, 3};
+    ASSERT_EQUAL(s.sortArrayByParity(v), expected);
+  }
+  {
+    vector<int> v = {3, 1, 2};
+    vector<int> expected = {2, 3, 1};
+    ASSERT_EQUAL(s.sortArrayByParity(v), expected);
+  }
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, testSolution);
+  return 0;
+}
